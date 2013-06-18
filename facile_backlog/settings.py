08adf6ce3039bb830217031676e2e9c1953ad032
David Saradini <david.saradini@me.com>
2013-06-19 01:14:00 +0200

    corrected ALLOWED_HOSTS
    Signed-off-by: David Saradini <david.saradini@me.com>

--- facile_backlog/settings.py
+++ facile_backlog/settings.py
@@ -44,7 +44,7 @@ PASSWORD_HASHERS = (
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["backlogman.com", "*.backlogman.com"]
+ALLOWED_HOSTS = ["backlogman.com", ".backlogman.com"]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
