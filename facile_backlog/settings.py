c2fece2cd87d8c5b25b87ab0873782e3c5968d74
David Saradini <david.saradini@me.com>
2013-06-18 23:50:43 +0200

    added static root
    Signed-off-by: David Saradini <david.saradini@me.com>

--- facile_backlog/settings.py
+++ facile_backlog/settings.py
@@ -75,7 +75,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.environ.get('STATIC_ROOT', os.path.join(HERE, 'public',
+                                                         'static'))
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
