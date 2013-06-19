2f68fb0cc5c1a17892ebdff57d2a9fc7b88b5ce5
David Saradini <david.saradini@me.com>
2013-06-19 10:07:45 +0200

    Will travis work
    Signed-off-by: David Saradini <david.saradini@me.com>

--- facile_backlog/test_settings.py
+++ facile_backlog/test_settings.py
@@ -1,3 +1,10 @@
 from settings import *  # noqa
 
 EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'backlog.sqlite',
+    }
+}
