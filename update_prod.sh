bb67260624b0a1a19236dfaf5c97e67ee3a78dd5
David Saradini <david.saradini@me.com>
2013-06-23 12:27:36 +0200

    Added noinput to migration script
    Signed-off-by: David Saradini <david.saradini@me.com>

--- update_prod.sh
+++ update_prod.sh
@@ -2,7 +2,7 @@ supervisorctl stop backlogman
 git pull
 source env/bin/activate
 envdir /etc/backlogman.d/ python manage.py migrate
-envdir /etc/backlogman.d/ python manage.py collectstatic
+envdir /etc/backlogman.d/ python manage.py collectstatic --noinput
 chmod -R 755 /home/backlogman/public
 supervisorctl start backlogman
 
