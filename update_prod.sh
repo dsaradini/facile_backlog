0adb1c183a577ac57d07f2d74680799f9a75b44c
David Saradini <david.saradini@me.com>
2013-06-24 22:37:27 +0200

    Corrected update script
    Signed-off-by: David Saradini <david.saradini@me.com>

--- update_prod.sh
+++ update_prod.sh
@@ -1,9 +1,8 @@
 supervisorctl stop backlogman
 git pull
-source env/bin/activate
-pip install -r requirements.txt
-envdir /etc/backlogman.d/ python manage.py migrate
-envdir /etc/backlogman.d/ python manage.py collectstatic --noinput
+env/bin/pip install -r requirements.txt
+envdir /etc/backlogman.d/ env/bin/python manage.py migrate
+envdir /etc/backlogman.d/ env/bin/python manage.py collectstatic --noinput
 chmod -R 755 /home/backlogman/public
 supervisorctl start backlogman
 
