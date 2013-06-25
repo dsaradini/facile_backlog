3d8a33fecff9c1a2365ed072f6faa023caa2e523
David Saradini <david.saradini@me.com>
2013-06-25 21:56:04 +0200

    Added info on REST API.
    Signed-off-by: David Saradini <david.saradini@me.com>

--- facile_backlog/__init__.py
+++ facile_backlog/__init__.py
@@ -1,3 +1,6 @@
+import os
+import subprocess
+
 VERSION = (0, 5)
 
 
@@ -5,6 +8,20 @@ def get_version():
     return ".".join(map(str, VERSION))
 
 __version__ = get_version()
+
+git_version = None
+
+if git_version is None:
+    root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                            os.pardir, os.pardir))
+    if os.path.isdir(os.path.join(root_dir, '.git')):
+        rev = subprocess.Popen(['git rev-parse --short HEAD'],
+                               shell=True, stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE,
+                               cwd=root_dir).communicate()[0].strip()
+        git_version = 'v{0}-{1}'.format(__version__, rev)
+    else:
+        git_version = __version__
 
 try:
     # don't break setup.py if django hasn't been installed yet
--- facile_backlog/api/serializers.py
+++ facile_backlog/api/serializers.py
@@ -1,31 +1,14 @@
 from rest_framework import serializers
 from rest_framework.reverse import reverse
 
-from ..backlog.models import Project, Backlog, UserStory
+from ..backlog.models import (Project, Backlog, UserStory)
+from ..core.models import User
 
 
-class ProjectSerializer(serializers.ModelSerializer):
-    url = serializers.SerializerMethodField('_url')
-
+class MemberSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Project
-        fields = ('id', 'url', 'name', 'code', 'description')
-
-    def _url(self, obj):
-        return reverse("api_project_detail", args=[obj.pk],
-                       request=self.context['request'])
-
-
-class BacklogSerializer(serializers.ModelSerializer):
-    url = serializers.SerializerMethodField('_url')
-
-    class Meta:
-        model = Backlog
-        fields = ('id', 'url', 'name', 'description')
-
-    def _url(self, obj):
-        return reverse("api_backlog_detail", args=[obj.project_id, obj.pk],
-                       request=self.context['request'])
+        model = User
+        fields = ('full_name', 'email',)
 
 
 class StorySerializer(serializers.ModelSerializer):
@@ -43,4 +26,36 @@ class StorySerializer(serializers.ModelSerializer):
     def _url(self, obj):
         return reverse("api_story_detail",
                        args=[obj.project_id, obj.backlog_id, obj.pk],
+                       request=self.context['request'])
+
+
+class BacklogSerializer(serializers.ModelSerializer):
+    url = serializers.SerializerMethodField('_url')
+    story_count = serializers.SerializerMethodField('_story_count')
+
+    class Meta:
+        model = Backlog
+        fields = ('id', 'url', 'name', 'description', 'story_count')
+
+    def _url(self, obj):
+        return reverse("api_backlog_detail", args=[obj.project_id, obj.pk],
+                       request=self.context['request'])
+
+    def _story_count(self, obj):
+        return obj.stories.count()
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    url = serializers.SerializerMethodField('_url')
+    users = MemberSerializer(many=True, allow_add_remove=False)
+    backlogs = BacklogSerializer(many=True)
+
+    class Meta:
+        model = Project
+        fields = ('id', 'url', 'name', 'code', 'description', 'users',
+                  'backlogs')
+        depth = 1
+
+    def _url(self, obj):
+        return reverse("api_project_detail", args=[obj.pk],
                        request=self.context['request'])
