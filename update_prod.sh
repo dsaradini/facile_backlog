73667bb7b35ac3b4d0ac71ad2092f96c22848f10
David Saradini <david.saradini@me.com>
2013-07-19 17:15:58 +0200

    update script changed to chmod
    Signed-off-by: David Saradini <david.saradini@me.com>

--- update_prod.sh
+++ update_prod.sh
@@ -5,6 +5,7 @@ env/bin/pip install -r requirements.txt
 envdir /etc/backlogman.d/ env/bin/python manage.py migrate
 envdir /etc/backlogman.d/ env/bin/python manage.py collectstatic --noinput
 chmod -R 755 /home/backlogman/public
+chown -R backlogman:nogroup /home/backlogman/public
 supervisorctl start backlogman
 supervisorctl start tornado
 
