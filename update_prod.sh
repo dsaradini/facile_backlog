e27dd8f370208d5ef22bdcd247848b2224a8d540
David Saradini <david.saradini@me.com>
2013-07-19 17:11:59 +0200

    Updated admin script to restart tornado
    Signed-off-by: David Saradini <david.saradini@me.com>

--- update_prod.sh
+++ update_prod.sh
@@ -1,8 +1,10 @@
 supervisorctl stop backlogman
+supervisorctl stop tornado
 git pull
 env/bin/pip install -r requirements.txt
 envdir /etc/backlogman.d/ env/bin/python manage.py migrate
 envdir /etc/backlogman.d/ env/bin/python manage.py collectstatic --noinput
 chmod -R 755 /home/backlogman/public
 supervisorctl start backlogman
+supervisorctl start tornado
 
