085eb2c6941736fa6b5a1433287d790da38acbfe
David Saradini <david.saradini@me.com>
2013-08-24 14:55:38 +0200

    Working with selenium tests
    Signed-off-by: David Saradini <david.saradini@me.com>

--- tests/test_storymap.py
+++ tests/test_storymap.py
@@ -1,6 +1,7 @@
 import time
 
 from django.core.urlresolvers import reverse
+from selenium.webdriver import ActionChains
 
 from . import SeleniumTestCase
 from . import factories
@@ -32,7 +33,7 @@ class StoryMapText(SeleniumTestCase):
         # Create 2 themes
         for seq in range(2):
             self.sel_query("a.create_theme").click()
-            time.sleep(0.1)
+            time.sleep(0.2)
             theme_input = self.sel_query("#theme-create-panel textarea")
             theme_input.send_keys("Theme {0}".format(seq))
             self.sel_query("#create-theme-btn").click()
@@ -40,10 +41,46 @@ class StoryMapText(SeleniumTestCase):
         # Create 2 phase
         for seq in range(2):
             self.sel_query("a.create_phase").click()
-            time.sleep(0.1)
+            time.sleep(0.2)
             theme_input = self.sel_query("#phase-create-panel textarea")
             theme_input.send_keys("Phase {0}".format(seq))
             self.sel_query("#create-phase-btn").click()
             time.sleep(0.5)
+        # let's move to the theme and phase to display the "new story" button
+        actionChains = ActionChains(self.browser)
+        actionChains.move_to_element(self.sel_query(".stories-zone"))
+        actionChains.click(self.sel_query(".create_story"))
+        actionChains.send_keys("My first story\n")
+        actionChains.perform()
 
-        time.sleep(10.2)
+        actionChains = ActionChains(self.browser)
+        actionChains.move_to_element(self.sel_query(".stories-zone"))
+        actionChains.click(self.sel_query(".create_story"))
+        actionChains.send_keys("My second story\n")
+        actionChains.perform()
+
+        # move story to another phase
+        actionChains = ActionChains(self.browser)
+        actionChains.drag_and_drop(
+            self.sel_query('.story-cell[story-id="1"]'),
+            self.sel_query('.stories-zone[phase-id="2"]'),
+        )
+        actionChains.perform()
+
+        # move theme
+        actionChains = ActionChains(self.browser)
+        actionChains.click_and_hold(self.sel_query('th[theme-id="1"]'))
+        actionChains.move_by_offset(300, 0)
+        actionChains.release()
+        actionChains.perform()
+
+        time.sleep(1)
+
+        # move theme
+        actionChains = ActionChains(self.browser)
+        actionChains.click_and_hold(self.sel_query('.phase-cell[phase-id="1"]'))
+        actionChains.move_by_offset(3, 200)
+        actionChains.release()
+        actionChains.perform()
+
+        time.sleep(1000.2)
