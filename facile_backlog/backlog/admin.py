9ab41d1263a82e1792f28ff10929e2d7726e6a8a
David Saradini <david.saradini@me.com>
2013-08-26 11:54:15 +0200

    Remove pk in search story
    Signed-off-by: David Saradini <david.saradini@me.com>

--- facile_backlog/backlog/admin.py
+++ facile_backlog/backlog/admin.py
@@ -44,7 +44,7 @@ class UserStoryAdmin(admin.ModelAdmin):
     list_display = ("code", "text", "project", "theme", "backlog", "status")
     readonly_fields = ("number", )
     search_fields = ("project__name", "as_a", "i_want_to", "number", "theme",
-                     "project__code", "pk")
+                     "project__code")
     raw_id_fields = ("project", "backlog")
 
 
